20e06e49f94de77ff5a6841a693270af9784387b
Artem <artkuryshev@yandex.ru>
2018-07-26 15:50:09 +0300

    Version 1.0.

---
+++ viselitsa.py
@@ -0,0 +1,40 @@
+def hangman(word):
+    wrong  = 0
+    stages = ["",
+             "________        ",
+             "|               ",
+             "|        |      ",
+             "|        0      ",
+             "|       /|\     ",
+             "|       / \     ",
+             "|               "
+              ]
+    rletters = list(word)
+    board = ["_"] * len(word)
+    win = False
+    print("Добро пожаловать на казнь!")
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "Введите букву: "
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+        print((" ".join(board)))
+        e = wrong + 1
+        print("\n".join(stages[0: e]))
+        if "_" not in board:
+            print("Вы выиграли! Было загадано слово: ")
+            print(" ".join(board))
+            win = True
+            break
+
+    if not win:
+          print("\n".join(stages[0: wrong]))
+          print("Вы прогирали! Было загадано слово: {}.".format(word))
+import random
+key = ["кот", "жесть", "мама", "Футбол"]
+hangman(random.choice(key))          
